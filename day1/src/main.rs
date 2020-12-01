5e48e53c7cebf4e7a7eea6f70551d75fc7921bc4
Bart Willems <bwillems@protonmail.com>
2020-12-01 18:53:07 +0100

    made day1 faster
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -14,25 +14,19 @@ fn main() {
 
     let now = std::time::Instant::now();
     let pair = find_pairs(&numbers);
-    println!(
-        "matching pair {:?} in {} nano seconds",
-        pair,
-        now.elapsed().as_nanos()
-    );
+    let elapsed = now.elapsed().as_micros();
+    println!("matching pair {:?} in {} microseconds", pair, elapsed);
 
     let now = std::time::Instant::now();
     let res = find_tripples(&numbers);
+    let elapsed = now.elapsed().as_micros();
 
-    println!(
-        "matching tripples: {:?} in {} nanoseconds",
-        res,
-        now.elapsed().as_nanos()
-    );
+    println!("matching tripples: {:?} in {} microseconds", res, elapsed);
 }
 
 fn find_pairs(numbers: &[u16]) -> (u16, u16) {
-    for (first_index, number) in numbers.iter().enumerate() {
-        for possible_match in numbers.iter().skip(first_index) {
+    for (index, number) in numbers.iter().enumerate() {
+        for possible_match in numbers.iter().skip(index) {
             if number + possible_match == 2020 {
                 return (*number, *possible_match);
             }
@@ -45,6 +39,9 @@ fn find_pairs(numbers: &[u16]) -> (u16, u16) {
 fn find_tripples(numbers: &[u16]) -> (u16, u16, u16) {
     for (first_index, number) in numbers.iter().enumerate() {
         for (second_index, number_two) in numbers.iter().enumerate().skip(first_index) {
+            if number + number_two >= 2020 {
+                continue;
+            }
             for possible_match in numbers.iter().skip(second_index) {
                 if number + number_two + possible_match == 2020 {
                     return (*number, *number_two, *possible_match);
