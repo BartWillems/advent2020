a473d3b3b2a70a387e2812a2ba02fbccb9e42c3d
Bart Willems <bwillems@protonmail.com>
2020-12-02 13:16:56 +0100

    added day2
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

---
+++ day2/src/main.rs
@@ -0,0 +1,123 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+use smallvec::SmallVec;
+
+struct Password<'a> {
+    token: u8,
+    min: usize,
+    max: usize,
+    password: &'a [u8],
+}
+
+impl<'a> Password<'a> {
+    fn from_string(input: &'a str) -> Self {
+        // SAFETY: this should not fail as long as the correct code advent day 2 2020 input is used
+        // otherwise, bad things will happen
+        unsafe {
+            let pairs: SmallVec<[&str; 3]> = input.split_whitespace().collect();
+
+            let min_max: SmallVec<[&str; 2]> = pairs.get_unchecked(0).split("-").collect();
+
+            let min: usize = min_max.get_unchecked(0).parse().unwrap();
+            let max: usize = min_max.get_unchecked(1).parse().unwrap();
+
+            let token: u8 = *pairs.get_unchecked(1).as_bytes().get_unchecked(0);
+
+            let password = pairs.get_unchecked(2);
+
+            Password {
+                token,
+                min,
+                max,
+                password: password.as_bytes(),
+            }
+        }
+    }
+
+    #[inline(always)]
+    fn is_valid_part_one(&self) -> bool {
+        let mut token_count = 0;
+
+        for t in self.password {
+            if t == &self.token {
+                token_count += 1;
+            }
+        }
+
+        token_count >= self.min && token_count <= self.max
+    }
+
+    #[inline(always)]
+    fn is_valid_part_two(&self) -> bool {
+        // SAFETY: this should not fail as long as the correct code advent day 2 2020 input is used
+        // otherwise, bad things will happen
+        unsafe {
+            let min = self.password.get_unchecked(self.min - 1) == &self.token;
+            let max = self.password.get_unchecked(self.max - 1) == &self.token;
+
+            min ^ max
+        }
+    }
+}
+
+fn main() {
+    let file = File::open("input.txt").unwrap();
+
+    let reader: BufReader<File> = BufReader::new(file);
+
+    let lines: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+
+    let now = std::time::Instant::now();
+    let passwords: SmallVec<[Password; 1024]> = lines
+        .iter()
+        .map(|line| Password::from_string(line))
+        .collect();
+    let count = correct_password_count_part_one(&passwords);
+    let elapsed = now.elapsed();
+
+    println!(
+        "part 1: found {} correct passwords in {} microseconds",
+        count,
+        elapsed.as_micros()
+    );
+
+    let now = std::time::Instant::now();
+    let passwords: SmallVec<[Password; 1024]> = lines
+        .iter()
+        .map(|line| Password::from_string(line))
+        .collect();
+    let count = correct_password_count_part_two(&passwords);
+    let elapsed = now.elapsed();
+
+    println!(
+        "part 2: found {} correct passwords in {} microseconds",
+        count,
+        elapsed.as_micros()
+    );
+}
+
+fn correct_password_count_part_one(passwords: &[Password]) -> u32 {
+    let mut count = 0;
+
+    for password in passwords {
+        if password.is_valid_part_one() {
+            count += 1;
+        }
+    }
+
+    count
+}
+
+fn correct_password_count_part_two(passwords: &[Password]) -> u32 {
+    let mut count = 0;
+
+    for password in passwords {
+        if password.is_valid_part_two() {
+            count += 1;
+        }
+    }
+
+    count
+}
